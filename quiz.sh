def87797034f88b0cd4844e3b6b3a9d953eaee7c
thejoelhansen <thejoelhansen@gmail.com>
2023-08-23 07:54:03 -0700

    Initial quiz script, add ex200_ch19

--- quiz.sh
+++ quiz.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 
 # Import config
-. dynDNS.conf
+. conf 
 
-date=`date`
+# Get number of questions in a supplied chapter, allow specify # of loops and specific chapter - default all chapters? 
 
-# Fix - TODO broken log path?
-#log="$installPath/dynDNS.log"
-log=/app/dynDNS/dynDNS.log
+ch1='/home/joel/projects/studyBuddy/chapters/ex200_ch1'
+rngStart=$(head $ch1 -c 1)
+rngStop=$(tail $ch1 -n 1)
 
-ipUrl=https://ifconfig.me
+echo $rngStart
+echo $rngStop
 
-# Get current IP and DNS, log them
+exit 0 
+# Randomly select a number
 
-currentIp=$(curl $ipUrl -s)
-currentDns=$(dig $DNS +short)
+# Print Question to STDOUT
 
-echo $date "Host has IP of" $currentIp "and" $DNS "resolves to" $currentDns >> $log
-
-# Compare current IP and DNS
-if [ $currentIp == $currentDns ] 
-then
-	echo $date "IPs match - no need to update" >> $log
-	exit 0
-elif [ $currentIp != $currentDns ] 
-then	
-	# Update log that the DNS is out of sync
-	echo $date "IPs don't match - updating DNS..." >> $log
-
-	# remove old A record	
-	gcloud beta dns --project=$gcProject record-sets transaction start --zone=$gcZone >> $log
-	gcloud beta dns --project=$gcProject record-sets transaction remove $currentDns --name=$DNS. --ttl=60 --type=A --zone=$gcZone >> $log 
-	gcloud beta dns --project=$gcProject record-sets transaction execute --zone=$gcZone >> $log 
-
-	# create new A record
-	gcloud beta dns --project=$gcProject record-sets transaction start --zone=$gcZone >> $log 
-	gcloud beta dns --project=$gcProject record-sets transaction add $currentIp --name=$DNS. --ttl=60 --type=A --zone=$gcZone >> $log 
-	gcloud beta dns --project=$gcProject record-sets transaction execute --zone=$gcZone >> $log 
-
-	# Update log that DNS was updated and they are now in sync
-	echo $date $DNS "succesfully updated to" $currentIp >> $log
-	exit 0
-fi
+# On button press print A and E
