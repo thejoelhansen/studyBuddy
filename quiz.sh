30edb67768ec02ecddfcd9d8246b250d5bc90bdf
thejoelhansen <thejoelhansen@gmail.com>
2023-09-01 21:15:02 -0700

    quiz update - working!

--- quiz.sh
+++ quiz.sh
@@ -3,9 +3,9 @@
 # Import config
 . conf 
 
-# Get number of questions in a supplied chapter, allow specify # of loops and specific chapter - default all chapters? 
+# !Get number of questions in a supplied chapter, allow specify # of loops and specific chapter - default all chapters? 
 
-ch='/home/joel/projects/studyBuddy/chapters/ex200_ch3'
+ch="/home/joel/projects/studyBuddy/chapters/ex200_ch3"
 
 # !improve currently limited to 2 characters - make so that numbers are up until first letter (EG 121q -> 121)
 
@@ -14,21 +14,20 @@ lastQ=$(tail $ch -n 1)
 rngStop=${lastQ:0:2}
 
 # Get random number
-# rng=$[ $RANDOM % $rngStop + 1 ] 
+rng=$[ $RANDOM % $rngStop + 1 ] 
 
-# q=$(grep $rng $ch) 
+# Fetch equally random question
+q=$(grep -w $rng"q" $ch ) 
 
-#echo $q
+echo $q
 
-if [ -f $1 ]
-then
-	echo "$1 is a file"
-elif [ -d $1 ]
-then
-	echo "$1 is a directory"
-else
-	"I don't know what \$1 is"
-fi
+# prompt for return, then proceed
+
+# show answer
+
+# show extra if present
+
+# loop finite amount? Like enter loop amount at beginning? 
 
 exit 0 
 
