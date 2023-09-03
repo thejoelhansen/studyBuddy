8ff1a6211bb706a95198e2a2581a66f590d7475a
thejoelhansen <thejoelhansen@gmail.com>
2023-09-02 20:25:56 -0700

    quiz.sh feature complete!

--- quiz.sh
+++ quiz.sh
@@ -5,29 +5,49 @@
 
 # !Get number of questions in a supplied chapter, allow specify # of loops and specific chapter - default all chapters? 
 
-ch="/home/joel/projects/studyBuddy/chapters/ex200_ch3"
+# ch="/home/joel/projects/studyBuddy/chapters/ex200_ch3"
+ch=$installPath"chapters/"$testPath"_ch"$1
+
+# test install path - main if wrap of script
+
+if [ -e $ch ]; then
 
 # !improve currently limited to 2 characters - make so that numbers are up until first letter (EG 121q -> 121)
 
-lastQ=$(tail $ch -n 1)
+	lastQ=$(tail $ch -n 1)
 
-rngStop=${lastQ:0:2}
+	rngStop=${lastQ:0:2}
 
-# Get random number
-rng=$[ $RANDOM % $rngStop + 1 ] 
+	# Get # of questions
+	echo "How many?"
+	read numberQuestions
 
-# Fetch equally random question
-q=$(grep -w $rng"q" $ch ) 
+	# Loop through # of questions in given chapter
+	for (( COUNT=$numberQuestions; COUNT>0; COUNT-- 
+	)); do
 
-echo $q
+		# Get random number
+		rng=$[ $RANDOM % $rngStop + 1 ] 
 
-# prompt for return, then proceed
+		# Fetch equally random question
+		q=$(grep -w $rng"q" $ch ) 
+		a=$(grep -w $rng"a" $ch )
+		e=$(grep -w $rng"e" $ch )
+		
+		echo $q
+		read answer
+		echo $a
+		echo $e
+		echo "Any key to continue..."
+		read answer
 
-# show answer
+	done
 
-# show extra if present
-
-# loop finite amount? Like enter loop amount at beginning? 
+# closing fi from test path check
+else
+	echo "Can't find" $ch
+	exit 1
+fi
 
 exit 0 
 
