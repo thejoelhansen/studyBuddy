74b2ae1283f2dad32c51ecd5d3a48377e5190485
thejoelhansen <thejoelhansen@gmail.com>
2023-09-04 11:03:19 -0700

    ex200 Release v1.0 candidate

--- quiz.sh
+++ quiz.sh
@@ -3,16 +3,48 @@
 # Import config
 . conf 
 
-# !Get number of questions in a supplied chapter, allow specify # of loops and specific chapter - default all chapters? 
+# !! state --help as $1=subject folder && $2=chapter file
 
-# ch="/home/joel/projects/studyBuddy/chapters/ex200_ch3"
-ch=$installPath"chapters/"$testPath"_ch"$1
+# Get subject folder
+if [ -z $1 ]; then
+	echo "What subject? Must match quiz subject folder name"	
+	read subject
+else
+	subject=$1
+fi
 
-# test install path - main if wrap of script
+# Test subject folder exists 
+if [ -z $subject ]; then
+	echo "Subject not supplied - exiting..."
+	exit 1
+elif [ ! -d $subject ]; then
+	echo "Subject folder not found - exiting..."
+	exit 1
+fi
 
+# Get for chapter based on file
+if [ -z $2 ]; then
+	echo "What chapter? Press Return to randomize from ch1-ch99"
+	read chapter
+
+	# If empty let chapter be random
+	if [ -z $chapter ]; then
+		chapter=$(ls $installPath/$subject | sort -R | head -n 1)
+	fi		
+fi
+
+# Test chapter exists
+if [ ! -f $installPath/$subject/$chapter ]; then
+	echo "Chapter file not found - exiting..."
+fi
+
+# Full path to quiz & chapter
+ch=$installPath/$subject/$chapter
+
+# Test install path - main if wrap of script
 if [ -e $ch ]; then
 
-# !improve currently limited to 2 characters - make so that numbers are up until first letter (EG 121q -> 121)
+# !!improve currently limited to 2 char AKA 1q through 99q - make so that numbers are up until first letter (EG 121q -> 121)
 
 	lastQ=$(tail $ch -n 1)
 
@@ -21,7 +53,11 @@ if [ -e $ch ]; then
 	# Get # of questions
 	echo "How many?"
 	read numberQuestions
-
+	# Test empty - make endless 
+	if [ -z $numberQuestions ]; then
+		numberQuestions=500
+	fi
+	
 	# Loop through # of questions in given chapter
 	for (( COUNT=$numberQuestions; COUNT>0; COUNT-- 
 	)); do
@@ -50,9 +86,3 @@ else
 fi
 
 exit 0 
-
-# Randomly select a number
-
-# Print Question to STDOUT
-
-# On button press print A and E
